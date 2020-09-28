2f8fafcb6f7c5bc7c647b67d3babc3c39cf10dcf
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-09-27 20:44:02 -0600

    Added prompts, confirms and generatePassword function test

--- script.js
+++ script.js
@@ -3,6 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var letterCount = prompt("How many letters?");
+  var upperCase = confirm("Do you want to include upper case letters?");
+  var lowerCase = confirm("Do you want to include lower case letters?");
+  var numbers = confirm("Do you want to include numbers?");
+  var uniqueCharacters = confirm("Do you want to include unique characters?");
+
+  function generatePassword() {
+    if (letterCount > 0) {
+      console.log("letters: Yes");
+    } else {
+      console.log("letters: No");
+    }
+
+    if (upperCase == true) {
+      console.log("UC: Yes");
+    } else {
+      console.log("UC: No");
+    }
+
+    if (lowerCase == true) {
+      console.log("LC: Yes");
+    } else {
+      console.log("LC: No");
+    }
+
+    if (numbers == true) {
+      console.log("Num: Yes");
+    } else {
+      console.log("Num: No");
+    }
+
+    if (uniqueCharacters == true) {
+      console.log("Unique: Yes");
+    } else {
+      console.log("Unique: No");
+    }
+
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
